37a5b536882dd6e8d2ecec65dfc73645b7b844d1
Jose A Juarez <joseantonio.juarezg6@gmail.com>
2022-03-09 14:02:53 -0600

    Prueba1 bucket vars

---
+++ dealer/bucket.tf
@@ -0,0 +1,38 @@
+terraform {
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 3.0"
+        }
+    }
+}
+
+variable "AWS_ACCESS_KEY_ID" {}
+variable "AWS_SECRET_ACCESS_KEY" {}
+
+provider "aws" {
+    region = var.region
+        profile = "test_profile"
+    access_key =  var.AWS_ACCESS_KEY_ID
+    secret_key = var.AWS_SECRET_ACCESS_KEY
+}
+
+/*------------bucket------------*/
+
+resource "aws_s3_bucket" "bucket1" {
+  bucket = var.bucket
+  acl = "public-read"
+  website {
+      index_document = "index.html"
+  }
+}
+
+/*------------content------------*/
+
+resource "aws_s3_bucket_object" "object1" {
+  bucket = aws_s3_bucket.bucket1.bucket
+  key    = "index.html"
+  acl = "public-read"
+  content = var.content
+  content_type = "text/html"
+}
---
+++ dealer/vars3.tf
@@ -0,0 +1,12 @@
+variable "region" {
+    type = string
+    default = "us-east-2"
+}
+variable "bucket" {
+    type = string
+    default = "jesuswebbucket1"
+}
+
+variable "content" {    
+    type = string
+}   
